55941721a838c17ce5c30c8d482b7f8e6138020f
qiumehhh <67218983+qiumehhh@users.noreply.github.com>
2020-06-28 00:30:47 +0800

    Add files via upload

---
+++ test-1/sw.js
@@ -0,0 +1,48 @@
+let cacheName = 'online-portfolio';
+// let caches;
+let filesToCache = [
+  'aboutme-img.jpg',
+  'banner.JPG',
+  'style.css',
+  'script.js',
+  'manifest.json',
+  'index.html'
+]
+
+/* 
+start the service worker, when the user access
+the website online. This will add the all the files 
+listed in filesToCache to the browser cache.
+
+*/
+self.addEventListener('install', function(e){
+  console.log("on install")
+    console.log(cacheName);
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache){
+      console.log("Adding files to cache")
+      return cache.addAll(filesToCache)
+    })
+  )
+})
+
+/*
+If offline or if the file exists in the cache, then it will fetch the files from cache
+*/
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request,{
+        cacheName: cacheName
+    }).then(function(response){
+        console.log(response);
+        console.log("Fetching "+e.request.url);
+      return response || fetch (e.request)
+    })
+  )
+})
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
